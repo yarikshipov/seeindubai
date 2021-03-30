2b0326e401ac9457bd7e6b447e1f8904a059e06f
Алексей Шипов <yarik.shipov@mail.ru>
2021-03-30 04:38:38 +0300

    just release it

--- mybot.py
+++ mybot.py
@@ -117,9 +117,9 @@ if "HEROKU" in list(os.environ.keys()):
     @server.route("/")
     def webhook():
         bot.remove_webhook()
-        bot.set_webhook(url="https://secret-fortress-01929.herokuapp.com") # этот url нужно заменить на url вашего Хероку приложения
+        bot.set_webhook(url="https://secret-fortress-01929.herokuapp.com/") # этот url нужно заменить на url вашего Хероку приложения
         return "?", 200
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
 else:
     # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.  
     # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
